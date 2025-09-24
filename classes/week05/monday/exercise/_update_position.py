e546483f41a1934941a57d88090d5207862d1a75
Ashley-babson2025 <abraren1@babson.edu>
2025-09-24 01:35:37 +0000

    update

--- classes/week05/monday/exercise/_update_position.py
+++ classes/week05/monday/exercise/_update_position.py
@@ -1,11 +1,18 @@
 def _update_position(active_client, transaction):
     # helper function to only be used inside create_transaction    
-
+'''  
+For contribution: 
+- no positions exist (first contribution)
+- add to existing cash balance
+'''
     if transaction['type'] == 'CONTRIBUTION':
+        cash = none #first cash position, nothing there 
         if active_client['positions']:
             for position in active_client['positions']:                
                 if position['symbol'] == transaction['symbol']:
                     position['shares'] += transaction['shares']
+                    cash = position #making new position cash?
+                    break
         else:
             position = {'id': transaction['id'],            
             'shares': round(transaction['shares'], 2),
@@ -15,10 +22,21 @@ def _update_position(active_client, transaction):
 
             active_client['positions'].append(position)    # or client['positions].append(position)
     elif transaction['type'] == 'BUY':
-        pass
+        return NONE
     elif transaction['type'] == 'SELL':
-        pass
+        return NONE
+''' 
+For withdrawal:
+   - Check available cash balance.
+   - Reduce the cash position accordingly.
+'''
     elif transaction['type'] == 'WITHDRAWAL':
+        if active_client['positions']:
+                    for position in active_client['positions']:                
+                        if position['symbol'] >= transaction['symbol']:
+                            position['shares'] -= transaction['shares']
+                            cash = position #making new position cash?
+                            break
         pass
     else:
         print('FATAL ERROR: we should never get here _update_postion')
