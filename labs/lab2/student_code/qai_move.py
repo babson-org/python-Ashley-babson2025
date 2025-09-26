d6d58a7920ff84e3619becf7aae9ed24b8abdec6
Ashley-babson2025 <abraren1@babson.edu>
2025-09-26 01:37:43 +0000

    update

--- labs/lab2/student_code/qai_move.py
+++ labs/lab2/student_code/qai_move.py
@@ -1,5 +1,7 @@
 def ai_move(board: list[int]):
     """
+    ai_move(board): Chooses a random available move (used in easy mode).
+
         Simple AI that selects the first available cell.
         Remember board is a list that starts with items 1 - 9
         [1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -9,7 +11,21 @@ def ai_move(board: list[int]):
         
         so in this case your function should return 2
     """
+    '''
+    function: create AI that places an X as an opponent
+    steps:
+    #CANNOT = 10 OR -10 --> TAKE ABSOLUTE VALUE OF BOARD 
+    - Create outer loop to cycle through board
+    - Create loop to go thorugh each item in list 
+        - if != 10, replace list item with 10
+        - else, continue 
+    - Return index for first != 10 
+    '''
     # TODO: Loop through board
-    # TODO: Find the first index where abs(cell) != 10
-    # TODO: Return that index as the AI's move
-    pass
+    
+    for i in range (len(board)):
+        if abs(board[i]) != 10: # TODO: Find the first index where abs(cell) != 10
+            board[i] = 10
+            return i # TODO: Return that index as the AI's move
+        else:
+            continue
