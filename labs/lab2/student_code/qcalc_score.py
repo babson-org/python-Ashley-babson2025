fec228c30aa2a38905de3104ede98172616cf4bd
Ashley-babson2025 <abraren1@babson.edu>
2025-09-28 00:36:27 +0000

    update

--- labs/lab2/student_code/qcalc_score.py
+++ labs/lab2/student_code/qcalc_score.py
@@ -8,19 +8,51 @@ def calc_score(board: list[int]):
         if they add upto -30 return -30
         else return 0
     """
-     
+    '''
+    1 | 2 | 3
+    -----------
+    4 | 5 | 6
+    -----------
+    7 | 8 | 9
+    function: sums up rows, col, diagonals to determine winner mathematically 
+    process:
+        - set list index values to corrsponding letter --> a = item 1 in board 
+        - Sum of wins= absolute value is 30
+            line sum: 
+                - sum 1,2,3
+                - sum 4,5,6
+                - sum 7,8,9
+            column sum: 
+                - sum a,d,g
+                - sum b,e,h
+                - sum c,f,i
+            diagnol sum: 
+                - sum a,e,i
+                - sum c,e,g
+    ''' 
+    # TODO: Sum the values at board[a], board[b], board[c] 
+    # TODO: Return 30 if X wins, -30 if O wins otherwise do not return
+    # TODO: For each of the 8 ways to win
+    # TODO: Check the cells in each row, column, or diagonal using line_sum
+    # TODO: Return 0 if line_sum() didn't return 30 or -30
+   
+    winning_line = [(1,2,3), (4,5,6), (7,8,9), (1,4,7), (2,5,8), (3,6,9), (1,5,9), (3,5,7)]
+#checking winning lines
     def line_sum(a, b, c):
         '''
             line_sum takes 3 numbers and if the sum is either 30
             or -30 returns that sum otherwise do not return
-        '''         
-         
-        # TODO: Sum the values at board[a], board[b], board[c] 
-        # TODO: Return 30 if X wins, -30 if O wins otherwise do not return
-        pass
-     
-    # TODO: For each of the 8 ways to win
-    # TODO: Check the cells in each row, column, or diagonal using line_sum
-    # TODO: Return 0 if line_sum() didn't return 30 or -30
-    pass
+        '''          
+        total = board [a], board [b], board [c]
+        if total == 30:
+            return 30
+        elif total == -30: #elif just another if statement 
+            return -30 
+        return 0 
+#run through the winning lines of board to see if they sum 30/-30
+    for a , b , c in winning_line:
+        result = line_sum(a, b, c)
+        if result != 0:
+            return result 
+        return 0
 
