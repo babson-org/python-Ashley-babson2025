fefd91e2da5c2222093f44f884fe5d2a48d305c8
Ashley-babson2025 <abraren1@babson.edu>
2025-10-03 02:01:38 +0000

    UPDATE

--- labs/lab2/student_code/qplayer_move.py
+++ labs/lab2/student_code/qplayer_move.py
@@ -7,16 +7,22 @@ def player_move(board: list[int], score: dict[str, int]):
         are 10 and -10 depending on who goes first.
     """
     
-    prompt = "Select an empty cell (1-9): "
+    prompt = int(input ("Select an empty cell (1-9): "))
     while True:
         try:
-            # TODO: Convert input to integer
+            # TODO: Convert input to integer ^^^^DONE ABOVE^^^^^^
             # TODO: Validate move is in range and not taken
+            
+        #USING A BOOL STATEMENT IF TRUE, CONTINUE LOOP, IF FALSE, ADD VALUE 
+
             pass
         except ValueError:
             prompt = "Invalid input. Try again (1-9): "
             
     # TODO: Assign score['player'] to the selected cell on the board
+
+#REPLACE SELECTED INDEX WITH 10 FOR PLAYER MOVE - REMEMBER INDEX FOR THIS WHERE 1 IS REALLY 0
+
     # HINT: remember the board moves are 1 - 9 but the board indices are
     # 0 - 8
 pass
