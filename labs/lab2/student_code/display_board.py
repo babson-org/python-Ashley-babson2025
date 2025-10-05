e3c668d1a2310b3755a50856759c79a37f385044
Ashley-babson2025 <abraren1@babson.edu>
2025-10-05 19:36:33 +0000

    update

--- labs/lab2/student_code/display_board.py
+++ labs/lab2/student_code/display_board.py
@@ -5,26 +5,7 @@ def print_board(board: list[int]) -> None:
     Display the Tic-Tac-Toe board.
     Args:
         board: List of 9 integers (10 for X, -10 for O, 1–9 for open).
-    """
-    def cell(value: int) -> str:
-        if value == 10: return 'X'
-        elif value == -10: return 'O'
-        else: return str(value)
-
-    clear_screen()
-    print()
-
-    for row in range(3):
-        row_values = [cell(board[row * 3 + col]) for col in range(3)]
-        print('   |   |   ')
-        print(f' {row_values[0]} | {row_values[1]} | {row_values[2]} ')
-        print('   |   |   ')
-        if row < 2:
-            print('-----------')
-    print()
-    
-from utils import clear_screen
-def print_board(board: list[int]):
+    """  
     """
         Display the Tic-Tac-Toe board with human-friendly layout.
         Remember the board may look like:
@@ -53,9 +34,19 @@ def print_board(board: list[int]):
         pass
         
     clear_screen()
+    
 
     # TODO: Loop through rows
+    for row in range(3):
+        row_values = [cell(board[row * 3 + col]) for col in range(3)]
+        print('   |   |   ')
+        print(f' {row_values[0]} | {row_values[1]} | {row_values[2]} ')
+        print('   |   |   ')
+        if row < 2:
+            print('-----------')
+    print()
     # TODO: For each row, print formatted board row using cell()
+
     # HINT: For each row create a list of what should be printed
     #       row_values = [ 'X', '2', '3' ] call cell() each time to get each value
     #       add the board layout:
